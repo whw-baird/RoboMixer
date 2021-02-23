d826c0ced0e960008406ab9288a4d6f2980da39b
whw-baird <wbaird@chicagobooth.edu>
2021-02-23 01:57:10 +0000

    Cleaned up Robomix output controller, set up output pages

--- app/controllers/Robomix_controller.rb
+++ app/controllers/Robomix_controller.rb
@@ -11,16 +11,19 @@ class RobomixController < ApplicationController
   end
 
   def output
-    # GENERATE COCKTAIL FORMAT
+    # GENERATE COCKTAIL FORMAT AND BASE LIQUOR
     format_array = [1,2,3,4,5,6]
     mix_format = format_array.sample
     @robo_format = Format.where({ :id => mix_format }).at(0)
+
     liquor_array = [1,2,2,2,2,2,2,2,2,3,3,4,4,4,5,5,6,7,7,7,7,8,8,8,8,8,8,8,8,9,9,9,9,10,11]
     mix_liquor = liquor_array.sample
     @robo_liquor = Liquor.where({ :id => mix_liquor }).at(0)
+
     if mix_format == 1
       # 3-PART
-      #Liquor, bitters, modifier
+      # liquor, bitters, modifier
+
       bitters_array = [1,1,1,1,1,1,1,2,2,2,3,4,4,5,5,5,5,5,5,5,6,6,6,7]
       mix_bitters = bitters_array.sample
       @robo_bitters = Bitter.where({ :id => mix_bitters }).at(0)
@@ -36,7 +39,8 @@ class RobomixController < ApplicationController
           @robo_modifier = Modifier.where({ :id => mix_modifier }).at(0)
         end
       end
-      # render({ :template => "cocktails/three_part.html.erb" })
+
+      render({ :template => "Robomix/cocktails/three_part.html.erb" })
     elsif mix_format == 2
       # Fizz
       #Liquor, citrus, soda
@@ -48,18 +52,19 @@ class RobomixController < ApplicationController
       mix_soda = soda_array.sample
       @robo_soda = Soda.where({ :id => mix_soda }).at(0)
 
-      # render
+      render({ :template => "Robomix/cocktails/fizz.html.erb" })
     elsif mix_format == 3
       # Martini
-      #Liquor, modifier
+      # liquor, modifier
       modifier_array = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,9,9,10,10]
       mix_modifier = modifier_array.sample
-      Modifier.where({ :id => mix_modifier }).at(0)
+      @robo_modifier = Modifier.where({ :id => mix_modifier }).at(0)
 
-      # render
+      render({ :template => "Robomix/cocktails/martini.html.erb" })
     elsif mix_format == 4
       # Old Fashioned
-      #Liquor, sweetener, bitters
+      # liquor, sweetener, bitters
+
       bitters_array = [1,2,3,4]
       mix_bitters = bitters_array.sample
       @robo_bitters = Bitter.where({ :id => mix_bitters }).at(0)
@@ -67,22 +72,25 @@ class RobomixController < ApplicationController
       sweetener_array = [1,2,3]
       mix_sweetener = sweetener_array.sample
       @robo_sweetener = Sweetener.where({ :id => mix_sweetener }).at(0)
-      # render
+      
+      render({ :template => "Robomix/cocktails/old_fashioned.html.erb" })
     elsif mix_format == 5
       # Sidecar
-      #Liquor, citrus, modifier
+      # liquor, citrus, modifier
+
       citrus_array = [1,2,3,4,5]
       mix_citrus = citrus_array.sample
       @robo_citrus = Citrus.where({ :id => mix_citrus }).at(0)
 
       modifier_array = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,9,9,10,10]
       mix_modifier = modifier_array.sample
-      Modifier.where({ :id => mix_modifier }).at(0)
+      @robo_modifier = Modifier.where({ :id => mix_modifier }).at(0)
 
-      # render
+      render({ :template => "Robomix/cocktails/sidecar.html.erb" })
     else 
       # Sour
-      #liquor, sweetener, citrus
+      # liquor, sweetener, citrus
+
       sweetener_array = [1,2,3]
       mix_sweetener = sweetener_array.sample
       @robo_sweetener = Sweetener.where({ :id => mix_sweetener }).at(0)
@@ -90,10 +98,10 @@ class RobomixController < ApplicationController
       citrus_array = [1,1,1,2,2,2,3,4,4,4,5]
       mix_citrus = citrus_array.sample
       @robo_citrus = Citrus.where({ :id => mix_citrus }).at(0)
-      # render
+
+      render({ :template => "Robomix/cocktails/sour.html.erb" })
     end
 
-    render({ :template => "Robomix/output.html.erb" })
   end
 
 end
